7a6606c562fc75f85e78cba874cde3fbcc9efca8
Keresztúri Hanna <100832885+hancsika15@users.noreply.github.com>
2024-12-11 19:08:31 +0100

    Képek és kilépés gomb

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DisneyQuizGame));
             lblscore = new Label();
             label2 = new Label();
             label1 = new Label();
@@ -37,28 +38,32 @@
             character02 = new Button();
             character01 = new Button();
             lblTimer = new Label();
+            btnexit = new Button();
+            label3 = new Label();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // lblscore
             // 
-            lblscore.AutoSize = true;
-            lblscore.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 238);
-            lblscore.Location = new Point(447, 177);
-            lblscore.Margin = new Padding(2, 0, 2, 0);
+            lblscore.BackColor = Color.MistyRose;
+            lblscore.BorderStyle = BorderStyle.Fixed3D;
+            lblscore.Font = new Font("Times New Roman", 13.875F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            lblscore.Location = new Point(1644, 454);
+            lblscore.Margin = new Padding(6, 0, 6, 0);
             lblscore.Name = "lblscore";
-            lblscore.Size = new Size(144, 19);
+            lblscore.Size = new Size(503, 102);
             lblscore.TabIndex = 39;
-            lblscore.Text = "Eddig elért pontjaid:";
+            lblscore.Text = "Az eddig elért pontjaid: ";
+            lblscore.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // label2
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Times New Roman", 48F, FontStyle.Regular, GraphicsUnit.Point, 238);
-            label2.Location = new Point(618, 22);
-            label2.Margin = new Padding(2, 0, 2, 0);
+            label2.Location = new Point(1257, 135);
+            label2.Margin = new Padding(4, 0, 4, 0);
             label2.Name = "label2";
-            label2.Size = new Size(351, 73);
+            label2.Size = new Size(704, 145);
             label2.TabIndex = 38;
             label2.Text = "Disney Quiz";
             // 
@@ -67,20 +72,20 @@
             label1.Anchor = AnchorStyles.None;
             label1.AutoSize = true;
             label1.Font = new Font("Times New Roman", 28.125F, FontStyle.Regular, GraphicsUnit.Point, 238);
-            label1.Location = new Point(531, 95);
-            label1.Margin = new Padding(2, 0, 2, 0);
+            label1.Location = new Point(1325, 280);
+            label1.Margin = new Padding(4, 0, 4, 0);
             label1.Name = "label1";
-            label1.Size = new Size(540, 43);
+            label1.Size = new Size(572, 85);
             label1.TabIndex = 37;
-            label1.Text = "Találd ki, hogy ki látható a képen!";
+            label1.Text = "Kit látsz a képen?\r\n";
             // 
             // pictureBox1
             // 
             pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
-            pictureBox1.Location = new Point(11, 11);
-            pictureBox1.Margin = new Padding(2, 1, 2, 1);
+            pictureBox1.Location = new Point(265, 32);
+            pictureBox1.Margin = new Padding(4, 2, 4, 2);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(427, 472);
+            pictureBox1.Size = new Size(800, 1000);
             pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
             pictureBox1.TabIndex = 36;
             pictureBox1.TabStop = false;
@@ -89,10 +94,10 @@
             // 
             characterCorrect.BackColor = Color.MistyRose;
             characterCorrect.Font = new Font("Times New Roman", 24F, FontStyle.Bold);
-            characterCorrect.Location = new Point(809, 358);
-            characterCorrect.Margin = new Padding(2, 1, 2, 1);
+            characterCorrect.Location = new Point(1644, 821);
+            characterCorrect.Margin = new Padding(4, 2, 4, 2);
             characterCorrect.Name = "characterCorrect";
-            characterCorrect.Size = new Size(347, 118);
+            characterCorrect.Size = new Size(499, 205);
             characterCorrect.TabIndex = 35;
             characterCorrect.Text = "characterCorrect";
             characterCorrect.UseVisualStyleBackColor = false;
@@ -101,10 +106,10 @@
             // 
             character03.BackColor = Color.MistyRose;
             character03.Font = new Font("Times New Roman", 24F, FontStyle.Bold);
-            character03.Location = new Point(809, 222);
-            character03.Margin = new Padding(2, 1, 2, 1);
+            character03.Location = new Point(1644, 601);
+            character03.Margin = new Padding(4, 2, 4, 2);
             character03.Name = "character03";
-            character03.Size = new Size(347, 118);
+            character03.Size = new Size(499, 205);
             character03.TabIndex = 34;
             character03.Text = "character03";
             character03.UseVisualStyleBackColor = false;
@@ -113,10 +118,10 @@
             // 
             character02.BackColor = Color.MistyRose;
             character02.Font = new Font("Times New Roman", 24F, FontStyle.Bold);
-            character02.Location = new Point(447, 358);
-            character02.Margin = new Padding(2, 1, 2, 1);
+            character02.Location = new Point(1127, 821);
+            character02.Margin = new Padding(4, 2, 4, 2);
             character02.Name = "character02";
-            character02.Size = new Size(347, 118);
+            character02.Size = new Size(499, 205);
             character02.TabIndex = 33;
             character02.Text = "character02";
             character02.UseVisualStyleBackColor = false;
@@ -125,30 +130,56 @@
             // 
             character01.BackColor = Color.MistyRose;
             character01.Font = new Font("Times New Roman", 24F, FontStyle.Bold);
-            character01.Location = new Point(447, 222);
-            character01.Margin = new Padding(2, 1, 2, 1);
+            character01.Location = new Point(1127, 601);
+            character01.Margin = new Padding(4, 2, 4, 2);
             character01.Name = "character01";
-            character01.Size = new Size(347, 118);
+            character01.Size = new Size(499, 205);
             character01.TabIndex = 32;
             character01.Text = "character01";
             character01.UseVisualStyleBackColor = false;
             // 
             // lblTimer
             // 
-            lblTimer.AutoSize = true;
+            lblTimer.BackColor = Color.MistyRose;
+            lblTimer.BorderStyle = BorderStyle.Fixed3D;
             lblTimer.Font = new Font("Times New Roman", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 238);
-            lblTimer.Location = new Point(809, 177);
+            lblTimer.Location = new Point(1129, 453);
+            lblTimer.Margin = new Padding(6, 0, 6, 0);
             lblTimer.Name = "lblTimer";
-            lblTimer.Size = new Size(53, 22);
+            lblTimer.Size = new Size(503, 102);
             lblTimer.TabIndex = 40;
             lblTimer.Text = "timer";
+            lblTimer.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // btnexit
+            // 
+            btnexit.BackColor = Color.MistyRose;
+            btnexit.Font = new Font("Times New Roman", 18F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            btnexit.Location = new Point(28, 32);
+            btnexit.Name = "btnexit";
+            btnexit.Size = new Size(197, 994);
+            btnexit.TabIndex = 41;
+            btnexit.Text = "Kilépés";
+            btnexit.UseVisualStyleBackColor = false;
+            btnexit.Click += btnexit_Click;
+            // 
+            // label3
+            // 
+            label3.BackColor = Color.DimGray;
+            label3.Location = new Point(1129, 579);
+            label3.Name = "label3";
+            label3.Size = new Size(1014, 10);
+            label3.TabIndex = 42;
+            label3.Text = "label3";
             // 
             // DisneyQuizGame
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.RosyBrown;
-            ClientSize = new Size(1161, 489);
+            ClientSize = new Size(2156, 1043);
+            Controls.Add(label3);
+            Controls.Add(btnexit);
             Controls.Add(lblTimer);
             Controls.Add(lblscore);
             Controls.Add(label2);
@@ -159,7 +190,8 @@
             Controls.Add(character02);
             Controls.Add(character01);
             FormBorderStyle = FormBorderStyle.FixedSingle;
-            Margin = new Padding(2, 1, 2, 1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(4, 2, 4, 2);
             Name = "DisneyQuizGame";
             Text = "DisneyQuiz/Game";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
@@ -178,5 +210,7 @@
         private Button character01;
         private Button btnRandomCharacter;
         private Label lblTimer;
+        private Button btnexit;
+        private Label label3;
     }
 }
--- Form2.cs
+++ Form2.cs
@@ -20,7 +20,7 @@ public partial class DisneyQuizGame : Form
     private int score = 0;
 
     private Timer questionTimer;
-    private int timeRemaining = 20; // 20 másodperc időlimit
+    private int timeRemaining = 10;
 
     public DisneyQuizGame()
     {
@@ -42,14 +42,13 @@ public partial class DisneyQuizGame : Form
         StartNewQuestion(); // Játék indítása az ablak betöltésekor
 
     }
-
     private void QuestionTimer_Tick(object sender, EventArgs e)
     {
         timeRemaining--;
 
         if (timeRemaining > 0)
         {
-            lblTimer.Text = "Idő hátra: " + timeRemaining + " másodperc";
+            lblTimer.Text = timeRemaining + " másodperc";
         }
         else
         {
@@ -61,33 +60,67 @@ public partial class DisneyQuizGame : Form
             StartNewQuestion(); // Új kérdés indítása
         }
     }
-
-
     private void InitializeImageDictionary()
     {
         imageDictionary = new Dictionary<string, Image>();
 
-        string imagePath = "C:\\Users\\ny20keresztúrih\\Source\\Repos\\DisenyQuiz\\images\\";
+        string imagePath = "C:\\Users\\User\\Desktop\\DisenyQuiz\\images\\";
 
-        imageDictionary.Add("Aranyhaj", Image.FromFile(imagePath + "rapunzel.png"));
-        imageDictionary.Add("Csipkerózsika", Image.FromFile(imagePath + "csipkerozsika.png"));
-        imageDictionary.Add("Elsa", Image.FromFile(imagePath + "elsa.png"));
-        imageDictionary.Add("Hamupipőke", Image.FromFile(imagePath + "hamupipoke.png"));
-        imageDictionary.Add("Hófehérke", Image.FromFile(imagePath + "hofeherke.png"));
-        imageDictionary.Add("Kristoff", Image.FromFile(imagePath + "kristoff.png"));
-        imageDictionary.Add("Olaf", Image.FromFile(imagePath + "olaf.png"));
+        imageDictionary.Add("Abu", Image.FromFile(imagePath + "abu.jpg"));
+        imageDictionary.Add("Anna", Image.FromFile(imagePath + "anna.jpg"));
+        imageDictionary.Add("Aranyhaj", Image.FromFile(imagePath + "aranyhaj.jpg"));
+        imageDictionary.Add("Ariel", Image.FromFile(imagePath + "ariel.jpg"));
+        imageDictionary.Add("Bambi", Image.FromFile(imagePath + "bambi.jpg"));
+        imageDictionary.Add("Belle", Image.FromFile(imagePath + "belle.jpg"));
+        imageDictionary.Add("Bolt", Image.FromFile(imagePath + "bolt.jpg"));
+        imageDictionary.Add("Buzz Lightyear", Image.FromFile(imagePath + "buzzLightyear.jpg"));
+        imageDictionary.Add("Daisy", Image.FromFile(imagePath + "daisy.jpg"));
+        imageDictionary.Add("Elsa", Image.FromFile(imagePath + "elsa.jpg"));
+        imageDictionary.Add("Eperke", Image.FromFile(imagePath + "eperke.jpg"));
+        imageDictionary.Add("Fiona", Image.FromFile(imagePath + "fiona.jpg"));
+        imageDictionary.Add("Flynn Rider", Image.FromFile(imagePath + "flynnrider.jpg"));
+        imageDictionary.Add("Füles", Image.FromFile(imagePath + "fules.jpg"));
+        imageDictionary.Add("Gaston", Image.FromFile(imagePath + "gaston.jpg"));
+        imageDictionary.Add("Goofy", Image.FromFile(imagePath + "goofy.jpg"));
+        imageDictionary.Add("Hádész", Image.FromFile(imagePath + "hadesz.jpg"));
+        imageDictionary.Add("Hófehérke", Image.FromFile(imagePath + "hofeherke.jpg"));
+        imageDictionary.Add("Jafar", Image.FromFile(imagePath + "jafar.jpg"));
+        imageDictionary.Add("Jessie", Image.FromFile(imagePath + "jessie.jpg"));
+        imageDictionary.Add("Judy Hoops", Image.FromFile(imagePath + "judyhoops.jpg"));
+        imageDictionary.Add("Malacka", Image.FromFile(imagePath + "malacka.jpg"));
+        imageDictionary.Add("Maximus", Image.FromFile(imagePath + "maximus.jpg"));
+        imageDictionary.Add("Mézi", Image.FromFile(imagePath + "mezi.jpg"));
+        imageDictionary.Add("Mickey Mouse", Image.FromFile(imagePath + "mickeyMouse.jpg"));
+        imageDictionary.Add("Mike Wazowski", Image.FromFile(imagePath + "mikeWazowski.jpg"));
+        imageDictionary.Add("Moana", Image.FromFile(imagePath + "moana.jpg"));
+        imageDictionary.Add("Nick Wilde", Image.FromFile(imagePath + "nickwilde.jpg"));
+        imageDictionary.Add("Olaf", Image.FromFile(imagePath + "olaf.jpg"));
+        imageDictionary.Add("Pascal", Image.FromFile(imagePath + "pascal.jpg"));
+        imageDictionary.Add("Pluto", Image.FromFile(imagePath + "pluto.jpg"));
+        imageDictionary.Add("Pumbaa", Image.FromFile(imagePath + "pumbaa.jpg"));
+        imageDictionary.Add("Randall", Image.FromFile(imagePath + "randall.jpg"));
+        imageDictionary.Add("Remy", Image.FromFile(imagePath + "remy.jpg"));
+        imageDictionary.Add("Rex", Image.FromFile(imagePath + "rex.jpg"));
+        imageDictionary.Add("Rugó", Image.FromFile(imagePath + "rugo.jpg"));
+        imageDictionary.Add("Sebastian", Image.FromFile(imagePath + "sebastian.jpg"));
+        imageDictionary.Add("Shrek", Image.FromFile(imagePath + "shrek.jpg"));
+        imageDictionary.Add("Sully", Image.FromFile(imagePath + "sulley.jpg"));
+        imageDictionary.Add("Sven", Image.FromFile(imagePath + "sven.jpg"));
+        imageDictionary.Add("Szamár", Image.FromFile(imagePath + "szamar.jpg"));
+        imageDictionary.Add("Szörnyeteg", Image.FromFile(imagePath + "szornyeteg.jpg"));
+        imageDictionary.Add("Tigris", Image.FromFile(imagePath + "tigris.jpg"));
+        imageDictionary.Add("Timon", Image.FromFile(imagePath + "timon.jpg"));
+        imageDictionary.Add("Ursula", Image.FromFile(imagePath + "ursula.jpg"));
+        imageDictionary.Add("Woody", Image.FromFile(imagePath + "woody.jpg"));
     }
-
     private void InitializeDisneyCharacters()
     {
         // Lista feltöltése
         disneyCharacters = new List<string>
-            {
-                "Aranyhaj","Elsa","Anna","Olaf","Kristoff","Sven","Hófehérke","Hét törpe","Hamupipőke","Tündér keresztanya","Csipkerózsika","Flóra","Fána","Fiona","Belle","Szörnyeteg","Gaston","Dzsinn","Abu","Jafar","Szimba","Mufasa","Zordon","Timon","Pumbaa","Nala","Scar","Ariel","Sebastian","Flounder","Ursula","Eric herceg","Max kutya", "Pinokkió", "Dzsepettó", "Tündér", "Tücsök Tihamér", "Dumbo", "Timothy Egér", "Tinker Bell", "Pán Péter", "Hook kapitány", "John", "Michael", "Wendy", "Bagira", "Maugli", "Balu", "Sír Kán", "Kaa", "Jane", "Tarzan", "Clayton", "Kala", "Kerchak", "Robin Hood", "Kis John", "Lady Marian", "Sheriff", "Szultán", "Herkules", "Megara", "Phil", "Hádész", "Pegazus", "Zeusz", "Anna hercegnő", "Rapunzel", "Flynn Rider", "Maximus", "Pascal", "Mother Gothel", "Moana", "Maui", "Heihei", "Tamatoa", "Te Fiti", "Elsőszámú Asszony", "Bolt", "Mittens", "Rhino", "Vanellope", "Ralph", "Felix", "Sgt. Calhoun", "Cukorka Király", "Mike Wazowski", "Sully", "Boo", "Roz", "Randall", "Woody", "Buzz Lightyear", "Jessie", "Mr. Potato Head", "Rex", "Hamm", "Slinky", "Bo Peep", "Remy", "Linguini", "Colette", "Skinner", "Emile", "Gusteau", "Marlin", "Némó", "Dory", "Crush", "Bruce", "Sven", "Wall-E", "Eve", "Auto", "Captain McCrea", "Merida", "Fergus király", "Elinor királynő", "Triplets", "Bambi", "Thumper", "Virág", "Anyakutya", "Balto", "Marie", "Toulouse", "Berlioz", "O'Malley", "Donald kacsa", "Mickey egér", "Minnie egér", "Pluto", "Goofy", "Max Goof", "Pete", "Tiki Taki", "Kanga", "Tigris", "Malacka", "Füles", "Micimackó", "Eperke", "Maui", "Heihei", "Rex", "Ratatouille", "Aurora"
-            };
+        {
+                "Hét törpe","Tündérkeresztanya","Flóra","Fána","Dzsinn","Szimba","Mufasa","Zordon","Nala","Scar","Flounder","Eric herceg","Max kutya","Pinokkió","Dzsepettó","Tündér","Tücsök Tihamér","Dumbo","Timothy Egér","Tinker Bell","Pán Péter","Hook kapitány","John","Michael","Wendy","Bagira","Maugli","Balu","Sír Kán","Kaa","Jane","Tarzan","Clayton","Kala","Kerchak","Robin Hood","Kis John","Lady Marian","Sheriff","Szultán","Herkules","Megara","Phil","Pegazus","Zeusz","Anna hercegnő","Mother Gothel","Maui","Heihei","Tamatoa","Te Fiti","Elsőszámú Asszony","Mittens","Rhino","Vanellope","Ralph","Felix","Sgt. Calhoun","Cukorka Király","Boo","Roz","Mr. Potato Head","Hamm","Slinky","Bo Peep","Linguini","Colette","Skinner","Emile","Gusteau","Marlin","Némó","Dory","Crush","Bruce","Sven","Wall-E","Eve","Auto","Captain McCrea","Merida","Fergus király","Elinor királynő","Triplets","Thumper","Virág","Anyakutya","Balto","Marie","Toulouse","Berlioz","O'Malley","Donald kacsa","Minnie egér","Max Goof","Pete","Tiki Taki","Kanga","Micimackó","Ratatouille","Aurora","Kuzco","Kronk","Yzma","Pacha","Esmeralda","Quasimodo","Phoebus","Frollo","Clopin","Pocahontas","John Smith","Meeko","Ratcliffe","Thomas","Nakoma","Percy","Vanessa","Gideon","Honest John","Roo","Winnifred Banks","George Banks","Bert","Mary Poppins","Madame Medúza","Bianca","Bernard","Madame Mim","Arthur","Merlin","Archimedes","Madame Bonfamille","Edgar","Frou-Frou","Napoleon","Lafayette","Tiana","Naveen","Dr. Facilier","Charlotte","Mama Odie","Louis","Ray","Eudora","Big Daddy La Bouff","Scat Cat","Colonel Hathi","Winifred","King Louie","Akela","Raksha","Colonel","Seargent Tibbs","Lucy","Lord Henry Mystic","Albert","Rafiki","Sarabi","Kiara","Kovu","Vitani","Nuka","Ranjan","Zira","Tantor","Professor Ratigan","Basil","Dawson","Olivia Flaversham","Fidget"
+        };
     }
-
-
     private void ResetButtonColors()
     {
         character01.BackColor = SystemColors.Control;
@@ -95,7 +128,6 @@ public partial class DisneyQuizGame : Form
         character03.BackColor = SystemColors.Control;
         characterCorrect.BackColor = SystemColors.Control;
     }
-
     private void GenerateNewQuestion()
     {
         ResetButtonColors();
@@ -124,15 +156,13 @@ public partial class DisneyQuizGame : Form
 
         pictureBox1.Image = imageDictionary[correctAnswer];
     }
-
     private void StartNewQuestion()
     {
-        timeRemaining = 20; // Időzítő visszaállítása
-        lblTimer.Text = "Idő hátra: " + timeRemaining + " másodperc";
+        timeRemaining = 10;
+        lblTimer.Text = timeRemaining + " másodperc";
         questionTimer.Start();
-        GenerateNewQuestion(); // Új kérdés generálása
+        GenerateNewQuestion();
     }
-
     private void CharacterButton_Click(object sender, EventArgs e)
     {
         ResetButtonColors();
@@ -141,20 +171,27 @@ public partial class DisneyQuizGame : Form
         if (clickedButton != null && clickedButton.Text == correctAnswer)
         {
             score++;
-            clickedButton.BackColor = Color.Green;
-            lblscore.Text = "Az eddig elért pontjaid: " + score.ToString();
         }
         else
         {
             score--;
-            clickedButton.BackColor = Color.Red;
-            lblscore.Text = "Az eddig elért pontjaid: " + score.ToString();
         }
+
+        // Frissítsd a pontszámot a felületen
+        lblscore.Text = "Az eddig elért pontjaid: " + score.ToString();
 
         if (score < 0)
         {
-            questionTimer.Stop(); // Időzítő leállítása
+            questionTimer.Stop();
             MessageBox.Show("Vesztettél!", "Eredmény", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            this.Hide();
+            DisneyQuiz frm = new DisneyQuiz();
+            frm.Show();
+        }
+        else if (score == 15)
+        {
+            questionTimer.Stop();
+            MessageBox.Show("Nyertél!", "Eredmény", MessageBoxButtons.OK, MessageBoxIcon.Information);
             this.Hide();
             DisneyQuiz frm = new DisneyQuiz();
             frm.Show();
@@ -163,5 +200,14 @@ public partial class DisneyQuizGame : Form
         {
             StartNewQuestion(); // Új kérdés indítása helyes vagy helytelen válasz után
         }
+    }
+
+    private void btnexit_Click(object sender, EventArgs e)
+    {
+        questionTimer.Stop(); 
+        MessageBox.Show("Kiléptél a játékból!", "Eredmény", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        this.Hide();
+        DisneyQuiz frm = new DisneyQuiz();
+        frm.Show();
     }
 }
